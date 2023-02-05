fec911815977ae9a6376ffad42fbd38efede4c9f
nelly-yusnita <yusnitaanelly@gmail.com>
2023-02-05 10:40:28 +0700

    revisi landingpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,43 @@
-// ===== HEADER ===== //
+// ===== DARK LIGHT THEME ===== //
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-line';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
+
+if (selectedTheme) {
+	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+	themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+	document.body.classList.toggle(darkTheme)
+	themeButton.classList.toggle(iconTheme)
+
+	localStorage.setItem('selected-theme', getCurrentTheme())
+	localStorage.setItem('selected-icon', getCurrentIcon())
+})
+// ===== end dark light theme ===== //
+
+// ===== GALLERY ===== //
+const swiperGallery = new Swiper(".gallery__container", {
+	spaceBetween: 10,
+	grabCursor: true,
+	slidesPerView: 'auto',
+	loop: 'true',
+
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
+// ===== end gallery ===== //
+
+// ===== PLAYLIST ===== //
 // Date & time
 function updateClock(){
 	var now = new Date();
@@ -41,54 +80,13 @@ function initClock(){
 }
 // end date & time
 
-// Dark light theme
-const themeButton = document.getElementById('theme-button');
-const darkTheme = 'dark-theme';
-const iconTheme = 'ri-sun-line';
-
-const selectedTheme = localStorage.getItem('selected-theme');
-const selectedIcon = localStorage.getItem('selected-icon');
-
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
-
-if (selectedTheme) {
-	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-	themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
-}
-
-themeButton.addEventListener('click', () => {
-	document.body.classList.toggle(darkTheme)
-	themeButton.classList.toggle(iconTheme)
-
-	localStorage.setItem('selected-theme', getCurrentTheme())
-	localStorage.setItem('selected-icon', getCurrentIcon())
-})
-// end dark light theme
-// ===== end header ===== //
-
-// ===== SECOND SECTION ===== //
-// Gallery
-const swiperGallery = new Swiper(".gallery__container", {
-	spaceBetween: 10,
-	grabCursor: true,
-	// centeredSlides: true,
-	slidesPerView: 'auto',
-	// loop: 'true',
-
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-});
-// end gallery
-// Playlist
+// playlist music
 let allMusic = [
 	// 1. Taylor Swift - Style
 	{
 		name:"Style",
 		artist: "Taylor Swift",
-		img: "ts-style-img",
+		img: "music1",
 		src: "Taylor Swift - Style"
 	},
 
@@ -96,7 +94,7 @@ let allMusic = [
 	{
 		name:"You're On Your Own Kid",
 		artist: "Taylor Swift",
-		img: "ts-yoyok-img",
+		img: "music2",
 		src: "Taylor Swift - You re On Your Own Kid"
 	},
 
@@ -104,7 +102,7 @@ let allMusic = [
 	{
 		name:"Cardigan",
 		artist: "Taylor Swift",
-		img: "ts-cardigan-img",
+		img: "music3",
 		src: "Taylor Swift - Cardigan"
 	},
 
@@ -112,7 +110,7 @@ let allMusic = [
 	{
 		name:"White Flag",
 		artist: "Dido",
-		img: "dido-img",
+		img: "music4",
 		src: "Dido - White Flag"
 	},
 
@@ -120,7 +118,7 @@ let allMusic = [
 	{
 		name:"Remedi",
 		artist: "TULUS",
-		img: "tulus-img",
+		img: "music5",
 		src: "TULUS - Remedi"
 	},
 
@@ -128,7 +126,7 @@ let allMusic = [
 	{
 		name:"Tally",
 		artist: "BLACKPINK",
-		img: "bp-img",
+		img: "music6",
 		src: "BLACKPINK - Tally"
 	},
 
@@ -136,7 +134,7 @@ let allMusic = [
 	{
 		name:"Dear Name",
 		artist: "IU",
-		img: "iu-img",
+		img: "music7",
 		src: "IU - Dear Name"
 	},
 
@@ -144,29 +142,29 @@ let allMusic = [
 	{
 		name:"Evaluasi",
 		artist: "Hindia",
-		img: "hindia-img",
+		img: "music8",
 		src: "Hindia - Evaluasi"
 	},
 
-	// 9. Hindia - Evaluasi
+	// 9. The Rose - Definition of ugly
 	{
 		name:"Definition of ugly is",
 		artist: "The Rose",
-		img: "therose-img",
+		img: "music9",
 		src: "The Rose - Definition of ugly"
 	},
 
-	// 10. Nadin Amizah - Bertaut
+	// 10. Jehwi - Looks Like a Real Thing
 	{
-		name:"Bertaut",
-		artist: "Nadin Amizah",
-		img: "nadin-img",
-		src: "Nadin Amizah - Bertaut"
+		name:"Looks Like a Real Thing",
+		artist: "Jehwi",
+		img: "music10",
+		src: "Jehwi - Looks Like a Real Thing"
 	}
 ]
 
 const playlist = document.querySelector("#playlist"),
-		musicImg = playlist.querySelector(".playlist__img img"),
+		musicImg = playlist.querySelector(".playlist__img"),
 		musicName = playlist.querySelector(".song__title"),
 		musicArtist = playlist.querySelector(".song__artist"),
 		playPauseBtn = playlist.querySelector(".song__play-pause"),
@@ -180,153 +178,209 @@ let musicIndex = Math.floor((Math.random() * allMusic.length) + 1);
 isMusicPaused = true;
 
 window.addEventListener("load", ()=>{
-  loadMusic(musicIndex);
+	loadMusic(musicIndex);
 });
 
 function loadMusic(indexNumb){
-  musicName.innerText = allMusic[indexNumb - 1].name;
-  musicArtist.innerText = allMusic[indexNumb - 1].artist;
-  musicImg.src = `assets/img/${allMusic[indexNumb - 1].img}.jpg`;
-  mainAudio.src = `assets/audio/${allMusic[indexNumb - 1].src}.mp3`;
+	musicName.innerText = allMusic[indexNumb - 1].name;
+	musicArtist.innerText = allMusic[indexNumb - 1].artist;
+	musicImg.src = `assets/img/${allMusic[indexNumb - 1].img}.jpg`;
+	mainAudio.src = `assets/audio/${allMusic[indexNumb - 1].src}.mp3`;
 }
 
 //play music function
 function playMusic(){
-  playlist.classList.add("paused");
-  playPauseBtn.querySelector("i").innerText = "pause";
-  mainAudio.play();
+	playlist.classList.add("paused");
+	playPauseBtn.querySelector("i").innerText = "pause";
+	mainAudio.play();
 }
 
 //pause music function
 function pauseMusic(){
-  playlist.classList.remove("paused");
-  playPauseBtn.querySelector("i").innerText = "play_arrow";
-  mainAudio.pause();
+	playlist.classList.remove("paused");
+	playPauseBtn.querySelector("i").innerText = "play_arrow";
+	mainAudio.pause();
 }
 
 //prev music function
 function prevMusic(){
-  musicIndex--; //decrement of musicIndex by 1
-  //if musicIndex is less than 1 then musicIndex will be the array length so the last music play
-  musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;
-  loadMusic(musicIndex);
-  playMusic();
+	musicIndex--; //decrement of musicIndex by 1
+	//if musicIndex is less than 1 then musicIndex will be the array length so the last music play
+	musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;
+	loadMusic(musicIndex);
+	playMusic();
 }
 
 //next music function
 function nextMusic(){
-  musicIndex++; //increment of musicIndex by 1
-  //if musicIndex is greater than array length then musicIndex will be 1 so the first music play
-  musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;
-  loadMusic(musicIndex);
-  playMusic(); 
+	musicIndex++; //increment of musicIndex by 1
+	//if musicIndex is greater than array length then musicIndex will be 1 so the first music play
+	musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;
+	loadMusic(musicIndex);
+	playMusic(); 
 }
 
 // play or pause button event
 playPauseBtn.addEventListener("click", ()=>{
-  const isMusicPlay = playlist.classList.contains("paused");
-  //if isPlayMusic is true then call pauseMusic else call playMusic
-  isMusicPlay ? pauseMusic() : playMusic();
+	const isMusicPlay = playlist.classList.contains("paused");
+	//if isPlayMusic is true then call pauseMusic else call playMusic
+	isMusicPlay ? pauseMusic() : playMusic();
 });
 
 //prev music button event
 prevBtn.addEventListener("click", ()=>{
-  prevMusic();
+	prevMusic();
 });
 
 //next music button event
 nextBtn.addEventListener("click", ()=>{
-  nextMusic();
+	nextMusic();
 });
 
 // update progress bar width according to music current time
 mainAudio.addEventListener("timeupdate", (e)=>{
-  const currentTime = e.target.currentTime; //getting playing song currentTime
-  const duration = e.target.duration; //getting playing song total duration
-  let progressWidth = (currentTime / duration) * 100;
-  progressBar.style.width = `${progressWidth}%`;
+	const currentTime = e.target.currentTime; //getting playing song currentTime
+	const duration = e.target.duration; //getting playing song total duration
+	let progressWidth = (currentTime / duration) * 100;
+	progressBar.style.width = `${progressWidth}%`;
 
-  let musicCurrentTime = playlist.querySelector(".song__current"),
-  musicDuartion = playlist.querySelector(".song__duration");
-  mainAudio.addEventListener("loadeddata", ()=>{
-    // update song total duration
-    let mainAdDuration = mainAudio.duration;
-    let totalMin = Math.floor(mainAdDuration / 60);
-    let totalSec = Math.floor(mainAdDuration % 60);
-    if(totalSec < 10){ //if sec is less than 10 then add 0 before it
-      totalSec = `0${totalSec}`;
-    }
-    musicDuartion.innerText = `${totalMin}:${totalSec}`;
-  });
-  // update playing song current time
-  let currentMin = Math.floor(currentTime / 60);
-  let currentSec = Math.floor(currentTime % 60);
-  if(currentSec < 10){ //if sec is less than 10 then add 0 before it
-    currentSec = `0${currentSec}`;
-  }
-  musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
+	let musicCurrentTime = playlist.querySelector(".song__current"),
+	musicDuartion = playlist.querySelector(".song__duration");
+	mainAudio.addEventListener("loadeddata", ()=>{
+		// update song total duration
+		let mainAdDuration = mainAudio.duration;
+		let totalMin = Math.floor(mainAdDuration / 60);
+		let totalSec = Math.floor(mainAdDuration % 60);
+		if(totalSec < 10){ //if sec is less than 10 then add 0 before it
+			totalSec = `0${totalSec}`;
+		}
+		musicDuartion.innerText = `${totalMin}:${totalSec}`;
+	});
+	// update playing song current time
+	let currentMin = Math.floor(currentTime / 60);
+	let currentSec = Math.floor(currentTime % 60);
+	if(currentSec < 10){ //if sec is less than 10 then add 0 before it
+		currentSec = `0${currentSec}`;
+	}
+	musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
 });
 
 // update playing song currentTime on according to the progress bar width
 progressArea.addEventListener("click", (e)=>{
-  let progressWidth = progressArea.clientWidth; //getting width of progress bar
-  let clickedOffsetX = e.offsetX; //getting offset x value
-  let songDuration = mainAudio.duration; //getting song total duration
-  
-  mainAudio.currentTime = (clickedOffsetX / progressWidth) * songDuration;
-  playMusic(); //calling playMusic function
+	let progressWidth = progressArea.clientWidth; //getting width of progress bar
+	let clickedOffsetX = e.offsetX; //getting offset x value
+	let songDuration = mainAudio.duration; //getting song total duration
+	
+	mainAudio.currentTime = (clickedOffsetX / progressWidth) * songDuration;
+	playMusic(); //calling playMusic function
 });
 
 //change loop, shuffle, repeat icon onclick
 const repeatBtn = playlist.querySelector("#repeat-playlist");
 repeatBtn.addEventListener("click", ()=>{
-  let getText = repeatBtn.innerText; //getting this tag innerText
-  switch(getText){
-    case "repeat":
-      repeatBtn.innerText = "repeat_one";
-      repeatBtn.setAttribute("title", "Song looped");
-      break;
-    case "repeat_one":
-      repeatBtn.innerText = "shuffle";
-      repeatBtn.setAttribute("title", "Playback shuffled");
-      break;
-    case "shuffle":
-      repeatBtn.innerText = "repeat";
-      repeatBtn.setAttribute("title", "Playlist looped");
-      break;
-  }
+	let getText = repeatBtn.innerText; //getting this tag innerText
+	switch(getText){
+		case "repeat":
+			repeatBtn.innerText = "repeat_one";
+			repeatBtn.setAttribute("title", "Song looped");
+			break;
+		case "repeat_one":
+			repeatBtn.innerText = "shuffle";
+			repeatBtn.setAttribute("title", "Playback shuffled");
+			break;
+		case "shuffle":
+			repeatBtn.innerText = "repeat";
+			repeatBtn.setAttribute("title", "Playlist looped");
+			break;
+	}
 });
 
 //code for what to do after song ended
 mainAudio.addEventListener("ended", ()=>{
-  // we'll do according to the icon means if user has set icon to
-  // loop song then we'll repeat the current song and will do accordingly
-  let getText = repeatBtn.innerText; //getting this tag innerText
-  switch(getText){
-    case "repeat":
-      nextMusic(); //calling nextMusic function
-      break;
-    case "repeat_one":
-      mainAudio.currentTime = 0; //setting audio current time to 0
-      loadMusic(musicIndex); //calling loadMusic function with argument, in the argument there is a index of current song
-      playMusic(); //calling playMusic function
-      break;
-    case "shuffle":
-      let randIndex = Math.floor((Math.random() * allMusic.length) + 1); //genereting random index/numb with max range of array length
-      do{
-        randIndex = Math.floor((Math.random() * allMusic.length) + 1);
-      }while(musicIndex == randIndex); //this loop run until the next random number won't be the same of current musicIndex
-      musicIndex = randIndex; //passing randomIndex to musicIndex
-      loadMusic(musicIndex);
-      playMusic();
-      break;
-  }
+	// we'll do according to the icon means if user has set icon to
+	// loop song then we'll repeat the current song and will do accordingly
+	let getText = repeatBtn.innerText; //getting this tag innerText
+	switch(getText){
+		case "repeat":
+			nextMusic(); //calling nextMusic function
+			break;
+		case "repeat_one":
+			mainAudio.currentTime = 0; //setting audio current time to 0
+			loadMusic(musicIndex); //calling loadMusic function with argument, in the argument there is a index of current song
+			playMusic(); //calling playMusic function
+			break;
+		case "shuffle":
+			let randIndex = Math.floor((Math.random() * allMusic.length) + 1); //genereting random index/numb with max range of array length
+			do{
+				randIndex = Math.floor((Math.random() * allMusic.length) + 1);
+			}while(musicIndex == randIndex); //this loop run until the next random number won't be the same of current musicIndex
+			musicIndex = randIndex; //passing randomIndex to musicIndex
+			loadMusic(musicIndex);
+			playMusic();
+			break;
+	}
 });
+// ===== end playlist ===== //
 
-// Blog
+// ===== MOVIES ===== //
+const moviesSwiper = new Swiper('.movie__slides', {
+	spaceBetween: 24,
+	loop: true,
+	grabCursor: true,
+
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+
+	breakpoints: {
+		467: {
+			slidesPerView: 2,
+		},
+
+		768: {
+			slidesPerView: 1,
+		},
+
+		992: {
+			slidesPerView: 2,
+		},
+	}
+});
+// ===== end movies ===== //
+
+// ===== BOOKS ===== //
+const booksSwiper = new Swiper('.books__slides', {
+	spaceBetween: 24,
+	loop: true,
+	grabCursor: true,
+
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+		dynamicBullets: true,
+	},
+
+	breakpoints: {
+		467: {
+			slidesPerView: 2,
+		},
+
+		768: {
+			slidesPerView: 1,
+		},
+
+		992: {
+			slidesPerView: 2,
+		},
+	}
+});
+// ===== end books ===== //
+
+// ===== BLOG ===== //
 const blogSwiper = new Swiper('.blog__container', {
 	spaceBetween: 24,
-	// loop: true,
+	loop: true,
 	grabCursor: true,
 
 	navigation: {
@@ -343,8 +397,7 @@ const blogSwiper = new Swiper('.blog__container', {
 		},
 	}
 });
-// end blog
-// ===== end second section ===== //
+// ===== end blog ===== //
 
 // ===== SCROLL UP ===== //
 function scrollUp(){
@@ -361,7 +414,9 @@ const sr = ScrollReveal({
 	distance: '60px',
 	duration: 2500,
 	delay: 400,
+	// reset: true// animation repeat //
 })
 
-sr.reveal(`.profile__border, .profile__name, .profile__subtitle, .profile__buttons, .content, .gallery, .playlist, .blog`);
-
+sr.reveal(`.section__title, .playlist__music-title, .blog__title, .bio__container, .gallery__container, .blog__container`)
+sr.reveal(`.content__container .content__box:first-child, .playlist__container .playlist__content:first-child, .movies__container .movie__content:first-child, .books__slides`, {origin: 'left'})
+sr.reveal(`.content__container .content__box:nth-child(2), .playlist__music, .movie__slides, .books__container .books__content:nth-child(2)`, {origin: 'right'})
